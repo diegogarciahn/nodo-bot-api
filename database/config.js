f2ea7fe5a476e21502ec4afa20dbf894196241e3
Diego Garcia <diegofabriciogarcia@gmail.com>
2023-04-15 09:39:08 -0600

    Configuración mongoose

---
+++ database/config.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+
+const dbConnection = async() => {
+    try {
+        
+        await mongoose.connect( process.env.DB_BOT);
+
+        console.log('DB Online');
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error en la base de datos - póngase en contacto con el equipo de soporte.');
+    }
+}
+
+module.exports = {
+    dbConnection
+}
