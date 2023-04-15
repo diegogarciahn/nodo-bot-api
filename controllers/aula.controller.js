7a2e9dc13f47fcb815ac66c33371fe35277121a7
Diego Garcia <diegofabriciogarcia@gmail.com>
2023-04-15 10:13:37 -0600

    controladores para aula y carrera

---
+++ controllers/aula.controller.js
@@ -0,0 +1,76 @@
+const Aula = require('../models/aula');
+
+// Crear nueva aula
+const createAula = async (req, res) => {
+    try {
+        const aula = new Aula(req.body);
+        await aula.save();
+        return res.status(201).json({ message: 'Aula creada con éxito', aula });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error al crear aula' });
+    }
+};
+
+// Obtener todas las aulas
+const getAllAulas = async (req, res) => {
+    try {
+        const aulas = await Aula.find();
+        return res.status(200).json(aulas);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error al obtener aulas' });
+    }
+};
+
+// Obtener una aula por ID
+const getAulaById = async (req, res) => {
+    try {
+        const aula = await Aula.findById(req.params.id);
+        if (!aula) {
+            return res.status(404).json({ message: 'Aula no encontrada' });
+        }
+        return res.status(200).json(aula);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error al obtener aula' });
+    }
+};
+
+// Actualizar una aula por ID
+const updateAulaById = async (req, res) => {
+    try {
+        const aula = await Aula.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        if (!aula) {
+            return res.status(404).json({ message: 'Aula no encontrada' });
+        }
+        return res.status(200).json({ message: 'Aula actualizada con éxito', aula });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error al actualizar aula' });
+    }
+};
+
+// Eliminar una aula por ID
+const deleteAulaById = async (req, res) => {
+    try {
+        const aula = await Aula.findByIdAndDelete(req.params.id);
+        if (!aula) {
+            return res.status(404).json({ message: 'Aula no encontrada' });
+        }
+        return res.status(200).json({ message: 'Aula eliminada con éxito', aula });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error al eliminar aula' });
+    }
+};
+
+module.exports = {
+    createAula,
+    getAllAulas,
+    getAulaById,
+    updateAulaById,
+    deleteAulaById,
+}
