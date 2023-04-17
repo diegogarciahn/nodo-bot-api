e730654b409f55a83af14fb0be8a7874d5027798
henrryJavinu1 <henryjavinu1@gmail.com>
2023-04-17 00:11:41 -0600

    actualizacion de rutas

--- routes/horario.routes.js
+++ routes/horario.routes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const {Router} = require('express');
+const router = Router();
 const { validarJWT } = require('../middlewares/validarJWT.middleware');
 const { crearHorario, obtenerHorarios, actualizarHorario, eliminarHorario  } = require('../controllers/horario.controller'); // Importar el controlador de horario
 
-router.post('/horario', [validarJWT], crearHorario); // Ruta para crear un nuevo horario
-router.get('/horario', [validarJWT], obtenerHorarios); // Ruta para obtener todos los horario
-router.put('/horario/:id', [validarJWT], actualizarHorario); // Ruta para actualizar un horario por su ID
-router.delete('/horario/:id', [validarJWT], eliminarHorario); // Ruta para borrar un horario por su ID
+router.post('/', crearHorario); // Ruta para crear un nuevo horario
+router.get('/', obtenerHorarios); // Ruta para obtener todos los horario
+router.put('/', actualizarHorario); // Ruta para actualizar un horario por su ID
+router.delete('/', eliminarHorario); // Ruta para borrar un horario por su ID
 
 module.exports = router;
