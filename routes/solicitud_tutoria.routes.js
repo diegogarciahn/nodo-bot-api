8ba99fa5aa246060210bde1bdfbb46b0a2a9edf7
Katia Aguilar <katyaguilargarcia@gmail.com>
2023-04-23 01:51:23 +0200

    cambio solicitudes

--- routes/solicitud_tutoria.routes.js
+++ routes/solicitud_tutoria.routes.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const { validarJWT } = require('../middlewares/validarJWT.middleware');
 const { crearSolicitudTutoria, obtenerSolicitudesTutorias, obtenerSolicitudTutoriaId, actualizarSolicitudTutoria, eliminarSolicitudTutoria } = require('../controllers/solicitud_tutoria.controller'); 
 
-router.post('/crearSolicitudTutoria', [
-    validarJWT,
-    check('estudiante', 'El estudiante debe ser obligatorio.').notEmpty(),
-    check('clase', 'La clase debe ser obligatoria.').notEmpty(),
-    check('horario_solicitado', 'El horario debe ser obligatorio.').notEmpty()
-],crearSolicitudTutoria); 
+router.post('/crearSolicitudTutoria', [validarJWT,],crearSolicitudTutoria); 
 router.get('/obtenerSolicitudesTutorias', [validarJWT], obtenerSolicitudesTutorias); 
 router.get('/obtenerSolicitudTutoria/:id', [validarJWT],obtenerSolicitudTutoriaId); 
 router.put('/actualizarSolicitudTutoria/:id', [validarJWT],actualizarSolicitudTutoria); 
