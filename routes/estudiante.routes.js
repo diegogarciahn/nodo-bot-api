99c067eeaee30eb0af566269da9ab6dfb131f5e9
Armando Euceda <euceda.armando98@gmail.com>
2023-04-23 08:48:51 -0600

    Ejemplo de validación de token y sanitización de datos.

--- routes/estudiante.routes.js
+++ routes/estudiante.routes.js
@@ -3,10 +3,25 @@ const { validarJWT } = require('../middlewares/validarJWT.middleware');
 const { crearEstudiante, obtenerEstudiantes, obtenerEstudiantePorId, actualizarEstudiante, borrarEstudiante } = require('../controllers/estudiante.controller'); // Importar el controlador de Estudiante
 const router = Router();
 
-router.post('/', [validarJWT], crearEstudiante); // Ruta para crear un nuevo estudiante
-router.get('/', [validarJWT], obtenerEstudiantes); // Ruta para obtener todos los estudiantes
-router.get('/search', [validarJWT], obtenerEstudiantePorId); // Ruta para obtener un estudiante por su ID
-router.put('/', [validarJWT], actualizarEstudiante); // Ruta para actualizar un estudiante por su ID
-router.delete('/', [validarJWT], borrarEstudiante); // Ruta para borrar un estudiante por su ID
+router.post('/', [
+    validarJWT,
+    
+], crearEstudiante); // Ruta para crear un nuevo estudiante
+router.get('/', [
+    validarJWT,
+    
+], obtenerEstudiantes); // Ruta para obtener todos los estudiantes
+router.get('/search', [
+    validarJWT,
+    
+], obtenerEstudiantePorId); // Ruta para obtener un estudiante por su ID
+router.put('/', [
+    validarJWT,
+    
+], actualizarEstudiante); // Ruta para actualizar un estudiante por su ID
+router.delete('/', [
+    validarJWT,
+    
+], borrarEstudiante); // Ruta para borrar un estudiante por su ID
 
 module.exports = router;
